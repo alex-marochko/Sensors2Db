048e307ad9a2e3b1e528df8eefea158079f1cc80
Alexander Marochko <marochko@gmail.com>
2015-12-04 15:31:44 +0200

    works for one sensor

--- app/src/main/java/name/marochko/sensors2db/FromSensorsToDB.java
+++ app/src/main/java/name/marochko/sensors2db/FromSensorsToDB.java
@@ -5,6 +5,7 @@ package name.marochko.sensors2db;
  */
 
 import android.app.IntentService;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -13,40 +14,184 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
 
 
-public class FromSensorsToDB extends IntentService{
+public class FromSensorsToDB extends IntentService implements SensorEventListener {
+
+    private final String LOG_TAG ="marinfo";
 
     private DBHelper dbHelper;
+    private SQLiteDatabase db;
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private Context context;
 
+    MyBinder binder = new MyBinder();
+
+    private boolean transaction_flag = false;
+    int count = 0;
+    long time = 0;
+
+    ContentValues contentValues = new ContentValues();
+
     public FromSensorsToDB(Context context){
-        super("myservice");
+        super("myService");
         this.context = context;
 
+        Log.d(LOG_TAG, "constructor FromSensorsToDB(Context context), context = " + context);
+    }
 
+    public FromSensorsToDB(String name){
+        super(name);
+        Log.d(LOG_TAG, "FromSensorsToDB(String name)");
+    }
+
+    public FromSensorsToDB(){
+        super("myService");
+        Log.d(LOG_TAG, "FromSensorsToDB()");
     }
 
 
-    public void startSensors(Context context){
+    public void startSensors(){
+        Log.d(LOG_TAG, "FromSensorsToDB.startSensors()");
 
-        dbHelper = new DBHelper(context);
-        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        db.beginTransaction();
+
+        transaction_flag = true;
+    }
+
+    public void stopSensors(){
+
+        Log.d(LOG_TAG, "FromSensorsToDB.stopSensors()");
+
+        mSensorManager.unregisterListener(this);
+
+        Log.d(LOG_TAG, "FromSensorsToDB.stopSensors()- unregisterListener ");
+
+        db.setTransactionSuccessful();
+
+        Log.d(LOG_TAG, "FromSensorsToDB.stopSensors()- setTransactionSuccessful() ");
+
+        db.endTransaction();
+        transaction_flag = false;
+        dbHelper.close();
+
+        stopSelf();
 
     }
+
+    public void readDB(){
+
+        Log.d(LOG_TAG, "FromSensorsToDB.readDB()");
+
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        Cursor c = db.rawQuery("select * from acceleration", null);
+        if (c != null) {
+            Log.d(LOG_TAG, "Records count = " + c.getCount());
+            if (c.moveToFirst()) {
+                do {
+                    Log.d(LOG_TAG, c.getString(c.getColumnIndex("x")));
+                } while (c.moveToNext());
+            }
+            c.close();
+        }
+
+        dbHelper.close();
+
+
+    }
+
+    public void clearDB(){
+
+        Log.d(LOG_TAG, "FromSensorsToDB.clearDB");
+
+        db = dbHelper.getWritableDatabase();
+
+        db.delete("acceleration", null, null);
+
+        dbHelper.close();
+
+
+    }
+
+
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+        Log.d(LOG_TAG, "FromSensorsToDB.onAccuracyChanged()");
+
+    }
+
+
+    public void onSensorChanged(SensorEvent event){
+
+        Log.d(LOG_TAG, "FromSensorsToDB.onSensorChanged");
+
+        if(!transaction_flag) return;
+
+        float x,y,z;
+
+        count++;
+
+        x = event.values[0];
+        y = event.values[1];
+        z = event.values[2];
+        time = event.timestamp;
+
+/*
+        sensorTextView.setText("x = " + x + "\ny = " + y + "\nz = " + z + "\ncount = " + count
+                + "\ntime = " + time);
+*/
+
+
+            contentValues.clear();
+
+            contentValues.put("x", x);
+            contentValues.put("y", y);
+            contentValues.put("z", z);
+            contentValues.put("time", time);
+
+            Log.d(LOG_TAG, contentValues.getAsString("x"));
+
+            db.insert("acceleration", null, contentValues);
+
+            Log.d(LOG_TAG, "record inserted!");
+    }
+
 
     @Override
     protected void onHandleIntent(Intent intent) {
+
+        Log.d(LOG_TAG, "FromSensorsToDB.onHandleIntent()");
 
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.d(LOG_TAG, "FromSensorsToDB.onStartCommand");
+
+//        startSensors();
+
+
+
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -58,17 +203,31 @@ public class FromSensorsToDB extends IntentService{
 
         public void onCreate(SQLiteDatabase db){
 
+            Log.d(LOG_TAG, "FromSensorsToDB.DBHelper.onCreate");
+
             db.execSQL("create table acceleration(" +
                             "id integer primary key autoincrement," +
                             "x real, y real, z real, time long" +
                             ");"
             );
         }
-
         public void onUpgrade(SQLiteDatabase db, int oldversion, int newversion){
 
-        }
+            Log.d(LOG_TAG, "FromSensorsToDB.DBHelper.onUpgrade");
 
+        }
+    }
+
+    public IBinder onBind(Intent arg0) {
+        Log.d(LOG_TAG, "MyService onBind");
+        return binder;
+    }
+
+    class MyBinder extends Binder {
+        FromSensorsToDB getService() {
+            Log.d(LOG_TAG, "MyBinder.getService()");
+            return FromSensorsToDB.this;
+        }
     }
 
 
--- app/src/main/java/name/marochko/sensors2db/MainActivity.java
+++ app/src/main/java/name/marochko/sensors2db/MainActivity.java
@@ -1,18 +1,36 @@
 package name.marochko.sensors2db;
 
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    final String LOG_TAG = "marinfo";
+    private boolean bound;
+    private TextView sensorTextView;
+    private Button btnStartRecording, btnStopRecording, btnClearDB, btnReadDB;
+    private FromSensorsToDB all_staff;// = new FromSensorsToDB(this);
+    private Intent intent;
+    ServiceConnection sConn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "MainActivity.onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -26,8 +44,51 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        intent = new Intent(this, FromSensorsToDB.class);
+
+        sConn = new ServiceConnection() {
+
+            public void onServiceConnected(ComponentName name, IBinder binder) {
+                Log.d(LOG_TAG, "MainActivity onServiceConnected");
+                all_staff = ((FromSensorsToDB.MyBinder) binder).getService();
+                bound = true;
+                Log.d(LOG_TAG, "MainActivity onServiceConnected all_staff: " + all_staff);
+            }
+
+            public void onServiceDisconnected(ComponentName name) {
+                Log.d(LOG_TAG, "MainActivity onServiceDisconnected");
+                bound = false;
+            }
+        };
+
+
+        btnStartRecording = (Button)findViewById(R.id.btnStartRecording);
+        btnStopRecording = (Button)findViewById(R.id.btnStopRecording);
+        btnClearDB = (Button)findViewById(R.id.btnClearDB);
+        btnReadDB  = (Button)findViewById(R.id.btnShowDB);
+
     }
 
+
+    @Override
+    protected void onStart() {
+        Log.d(LOG_TAG, "MainActivity.onStart");
+        super.onStart();
+        bindService(intent, sConn, 0);
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(LOG_TAG, "MainActivity.onStop");
+        super.onStop();
+        if (!bound) return;
+        unbindService(sConn);
+        bound = false;
+    }
+
+
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -49,4 +110,51 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+*/
+
+
+    public void onStartClick(View v) {
+        Log.d(LOG_TAG, "MainActivity.onStartClick");
+
+        btnStopRecording.setEnabled(true);
+        btnStartRecording.setEnabled(false);
+        btnClearDB.setEnabled(false);
+
+        startService(intent);
+
+//        all_staff.startSensors();
+    }
+
+    public void onStopClick(View v){
+        Log.d(LOG_TAG, "MainActivity.onStopClick");
+
+        all_staff.stopSensors();
+
+
+
+        btnStopRecording.setEnabled(false);
+        btnStartRecording.setEnabled(true);
+        btnClearDB.setEnabled(true);
+
+//        all_staff.stopService(intent);
+
+//        stopService(intent);
+
+    }
+
+    public void onShowClick(View v){
+        Log.d(LOG_TAG, "MainActivity.onShowClick");
+
+        all_staff.readDB();
+
+    }
+
+    public void onClearClick(View v){
+        Log.d(LOG_TAG, "MainActivity.onClearClick");
+
+        all_staff.clearDB();
+
+    }
+
+
 }
